5f98ee7aaf83d74aea534a6abeaa59023f47fa45
hasancoders09 <145287137+hasancoders09@users.noreply.github.com>
2024-01-24 04:42:39 +0600

    Update Main.java

--- 05 Input from user/Main.java
+++ 05 Input from user/Main.java
@@ -1,4 +1,14 @@
-//Call "Scanner" class from Java library
+// nextBoolean()	Reads a boolean value from the user
+// nextByte()	Reads a byte value from the user
+// nextDouble()	Reads a double value from the user
+// nextFloat()	Reads a float value from the user
+// nextInt()	Reads a int value from the user
+// nextLine()	Reads a String value from the user
+// nextLong()	Reads a long value from the user
+// nextShort()	Reads a short value from the user
+
+
+
 import java.util.Scanner;
 
 public class Main {
@@ -31,11 +41,3 @@ public static void main(String[] args) {
 }   
 }
 
-// nextBoolean()	Reads a boolean value from the user
-// nextByte()	Reads a byte value from the user
-// nextDouble()	Reads a double value from the user
-// nextFloat()	Reads a float value from the user
-// nextInt()	Reads a int value from the user
-// nextLine()	Reads a String value from the user
-// nextLong()	Reads a long value from the user
-// nextShort()	Reads a short value from the user
