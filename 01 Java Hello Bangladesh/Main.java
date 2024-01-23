0fec7dd9af0930916bc9b6e719ddeccd2e67c176
hasancoders09 <145287137+hasancoders09@users.noreply.github.com>
2024-01-24 00:43:51 +0600

    Update Main.java

--- 01 Java Hello Bangladesh/Main.java
+++ 01 Java Hello Bangladesh/Main.java
@@ -1,9 +1,12 @@
+//create class using same file name
 public class Main {
 
     //Create a method
     public static void main(String[] args) {
 
         //Write a print method for output
+
+	    // for print a string or text write that under the "" quotation. 
         System.out.println("Hello World");
 
         //print is used for output. Additionally add 'ln' for new line: println
