7f802ab4dc30c2417f84598a7901a7855034cbc3
hasancoders09 <145287137+hasancoders09@users.noreply.github.com>
2024-01-24 03:56:42 +0600

    Update Main.java

--- 05 Input from user/Main.java
+++ 05 Input from user/Main.java
@@ -5,12 +5,37 @@ public class Main {
 
 public static void main(String[] args) {
   //Declare the "Scanner" class
-  Scanner input = new Scanner(System.in);
+  Scanner $input = new Scanner(System.in);
+  Scanner $nameInput = new Scanner(System.in);
+  //Declare Veriable
   int number;
-  System.out.print("Enter a number: ");
+  String name;
+  
+  //Massage for Input data
+  System.out.print("Enter your age: ");
   //Call interger type data
-  number = input.nextInt();
-  System.out.println("Your entered number is: "+ number);
+  number = $input.nextInt();
+  
+
+  System.out.print("Enter your Name: ");
+  //Call String type data
+  //"next" is for only one word & "nextLine" For multiple word list or full input
+  name = $nameInput.nextLine();
+  
+  //Output data
+  System.out.println("Welcome "+ name);
+  System.out.println("Your age is: "+ number);
+  
+  
 
 }   
 }
+
+// nextBoolean()	Reads a boolean value from the user
+// nextByte()	Reads a byte value from the user
+// nextDouble()	Reads a double value from the user
+// nextFloat()	Reads a float value from the user
+// nextInt()	Reads a int value from the user
+// nextLine()	Reads a String value from the user
+// nextLong()	Reads a long value from the user
+// nextShort()	Reads a short value from the user
