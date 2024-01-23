d6bb1162a61085ab8461b293ce8e6dca9b825cd5
hasancoders09 <145287137+hasancoders09@users.noreply.github.com>
2024-01-24 00:53:15 +0600

    Update Main.java

--- 01 Java Hello Bangladesh/Main.java
+++ 01 Java Hello Bangladesh/Main.java
@@ -11,19 +11,6 @@ public class Main {
 
         //print is used for output. Additionally add 'ln' for new line: println
 		System.out.print("Hello Bangladesh");
-
-
-
-	// \t: It is used to insert a tab in the text.
-	// \b: It is used to insert a backspace in the text.
-	// \’: It is used to insert a single quote of character in the text.
-	// \f: It is used to insert a form feed in the text.
-	// \”: It is used to insert a double-quote character in the text.
-	// \n: It is used to insert a new line in the text.
-	// \r: It is used to insert a carriage return in the text.
-	// \: It is used to insert a backslash character.
-
-	System.out.print("Hello Bangladesh");
       }
     
 }
