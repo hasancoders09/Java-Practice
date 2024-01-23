d52ff410401fb6449fd8f7f107476dec237819ec
hasancoders09 <145287137+hasancoders09@users.noreply.github.com>
2024-01-24 03:34:51 +0600

    Update Main.java

--- 05 Input from user/Main.java
+++ 05 Input from user/Main.java
@@ -1,1 +1,16 @@
+//Call "Scanner" class from Java library
+import java.util.Scanner;
 
+public class Main {
+
+public static void main(String[] args) {
+  //Declare the "Scanner" class
+  Scanner input = new Scanner(System.in);
+  int number;
+  System.out.print("Enter a number: ");
+  //Call interger type data
+  number = input.nextInt();
+  System.out.println("Your entered number is: "+ number);
+
+}   
+}
