0fc63384d606e2c900d50db5bb1435275b008f5b
hasancoders09 <145287137+hasancoders09@users.noreply.github.com>
2024-01-24 02:00:22 +0600

    Update Main.java

--- 03 Primitive Data Types/Main.java
+++ 03 Primitive Data Types/Main.java
@@ -1,1 +1,28 @@
 
+public class Main {
+  
+ /* DataType    Size        Range */
+  
+// byte         1 byte      from -128 to 127
+byte $byteDtype = 5; 
+// Short        2 bytes     from -32,768 to 32,767
+short $shortDtype = 5; 
+// Integer      4 bytes     from -2,147,483,648 to 2,147,483,647
+int $intDtype = 5; 
+// Long         8 bytes     from -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807
+long $longDtype = 5; 
+//FloatingPoint 4 bytes     6 to 7 decimal digits
+float $floatDtype = 5.99f;    
+//Double        8 bytes     15 decimal digits
+double $doubleDtype = 5.99;    
+// Character    2 bytes     character/letter or ASCII values
+char $charDtype = 'D';         
+ // Boolean     1 byte      true or false values
+boolean $booleanDtype = true;      
+// String
+String $StringDtype = "Hello";     
+
+
+
+    
+}
