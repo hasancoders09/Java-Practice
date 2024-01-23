d796f8584feb9a91054c5497f84a3c5e910e71ee
hasancoders09 <145287137+hasancoders09@users.noreply.github.com>
2024-01-24 01:11:49 +0600

    Update Main.java

--- 02 Escape sequence/Main.java
+++ 02 Escape sequence/Main.java
@@ -1,1 +1,24 @@
+//create class using same file name
+public class Main {
 
+    //Create a method
+    public static void main(String[] args) {
+	// \t: It is used to insert a tab in the text.
+	System.out.println("Hello \t Bangladesh 1");
+	// \b: It is used to insert a backspace in the text.
+	System.out.println("Hello\b Bangladesh 2");
+	// \’: It is used to insert a single quote of character in the text.
+	System.out.println("Hello \'Bangladesh\' 3");
+		// \”: It is used to insert a double-quote character in the text.
+	System.out.println("Hello \"Bangladesh\" 4");
+	// \f: It is used to insert a form feed in the text.
+	System.out.println("Hello \f Bangladesh 5");
+	// \n: It is used to insert a new line in the text.
+	System.out.println("Hello\nBangladesh 6");
+	// \r: It is used to insert a carriage return in the text.
+	System.out.println("Hello \r Bangladesh 7");
+	// \\: It is used to insert a backslash character.
+	System.out.println("Hello \\ Bangladesh 8");
+      }
+    
+}
