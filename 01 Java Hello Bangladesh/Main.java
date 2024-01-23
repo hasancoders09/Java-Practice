282769deb45de493b1e790f8a9f463de4b01c12b
hasancoders09 <145287137+hasancoders09@users.noreply.github.com>
2024-01-24 00:39:52 +0600

    Update Main.java

--- 01 Java Hello Bangladesh/Main.java
+++ 01 Java Hello Bangladesh/Main.java
@@ -1,6 +1,13 @@
 public class Main {
+
+    //Create a method
     public static void main(String[] args) {
-        System.out.println("Hello Bangladesh");
+
+        //Write a print method for output
+        System.out.println("Hello World");
+
+        //print is used for output. Additionally add 'ln' for new line: println
+		System.out.print("Hello Bangladesh");
       }
     
 }
