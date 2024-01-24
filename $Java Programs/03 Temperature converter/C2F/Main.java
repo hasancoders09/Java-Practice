5b3bae8341d55ec6ab7ac293f421682a9cc12213
hasancoders09 <145287137+hasancoders09@users.noreply.github.com>
2024-01-25 00:45:44 +0600

    Update Main.java

--- $Java Programs/03 Temperature converter/C2F/Main.java
+++ $Java Programs/03 Temperature converter/C2F/Main.java
@@ -1,1 +1,17 @@
+/* ===============  Celsius to Fahrenheit ================ */
 
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        double $celsius, $result;
+        
+        System.out.print("Enter the celsius temperature: ");
+        $celsius = input.nextDouble();
+        //C 2 F formula (celsius * 9)/5 + 32
+        $result =   ($celsius * 9)/5 + 32;
+        
+        System.out.println("The Fahrenheit temperature is: " + $result);
+    }
+}
