30a35f40fed8cc21d223fb1d9fe27614c624d005
hasancoders09 <145287137+hasancoders09@users.noreply.github.com>
2024-01-24 23:56:31 +0600

    Update Main.java

--- $Java Programs/01 Area of circle/Main.java
+++ $Java Programs/01 Area of circle/Main.java
@@ -1,1 +1,22 @@
 
+/* ===============  Area of Circle ================ */
+
+
+
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        double $PI, $radius, $result;
+        
+        System.out.print("Enter the cricle radius: ");
+        $radius = input.nextDouble();
+        
+        $PI = 3.1416;
+        //Formula: πr^2
+        $result = $PI * $radius * $radius;
+        
+        System.out.println("The area of circle is: ≈" + $result);
+    }
+}
