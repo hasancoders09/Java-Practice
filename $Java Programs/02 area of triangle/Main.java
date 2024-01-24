87e6588b5dcc6a71fd79832ebfc9a938541983ce
hasancoders09 <145287137+hasancoders09@users.noreply.github.com>
2024-01-24 23:41:59 +0600

    Update Main.java

--- $Java Programs/02 area of triangle/Main.java
+++ $Java Programs/02 area of triangle/Main.java
@@ -1,1 +1,25 @@
 
+/* ===============  Aria of Triangle ================ */
+
+
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		double $base, $height, $result;
+		
+		System.out.print("Enter the triangle Base: ");
+		$base = input.nextDouble();
+		
+		System.out.print("Enter the triangle Height: ");
+		$height = input.nextDouble();
+		
+		//The formula aria of triangle is = 1/2 * base * height
+		$result = 0.5 * $base * $height;
+		
+		System.out.println("The aria of triangle is: " + $result);
+	}
+}
+
