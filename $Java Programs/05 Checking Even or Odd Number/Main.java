eb123ec22c3e9d58ebcf356214bba04780d54a82
hasancoders09 <145287137+hasancoders09@users.noreply.github.com>
2024-01-27 03:18:51 +0600

    Update Main.java

--- $Java Programs/05 Checking Even or Odd Number/Main.java
+++ $Java Programs/05 Checking Even or Odd Number/Main.java
@@ -1,1 +1,18 @@
 
+/*=====Check Even or Odd number Program, ( if,else ) Statement=====*/
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args){
+        Scanner $input = new Scanner(System.in);
+        int $number;
+        System.out.print("Enter a number: ");
+        $number = $input.nextInt();
+        
+        if($number % 2 == 0){
+            System.out.println("Even");
+        }
+        else{
+            System.out.println("Odd");
+        }
+    }
+}
