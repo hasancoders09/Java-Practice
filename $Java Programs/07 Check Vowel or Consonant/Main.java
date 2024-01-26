ba3b3759cc05abb0c2f33a21379ad0b3fd2c51ce
hasancoders09 <145287137+hasancoders09@users.noreply.github.com>
2024-01-27 04:11:37 +0600

    Update Main.java

--- $Java Programs/07 Check Vowel or Consonant/Main.java
+++ $Java Programs/07 Check Vowel or Consonant/Main.java
@@ -1,1 +1,18 @@
+/*========================Check Vowel or Consonant,(if, else) Statement =======================*/
 
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args){
+        Scanner $inputLetter = new Scanner(System.in);
+        char $letter;
+        System.out.print("Enter a single letter: ");
+        $letter = $inputLetter.next().charAt(0);
+        if($letter == 'a' | $letter == 'e' | $letter == 'i' | $letter == 'o' | $letter == 'u' |
+        $letter == 'A' | $letter == 'E' | $letter == 'I' | $letter == 'O' | $letter == 'U'){
+            System.out.println("Vowel");
+        }
+        else{
+            System.out.println("Consonant");
+        }
+    }
+}
