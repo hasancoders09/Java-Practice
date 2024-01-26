9b2ac5500d3ce2acdf538ba4470f161ff875250b
hasancoders09 <145287137+hasancoders09@users.noreply.github.com>
2024-01-27 02:49:22 +0600

    Update Main.java

--- $Java Programs/04 Check Positive or Negative Number/Main.java
+++ $Java Programs/04 Check Positive or Negative Number/Main.java
@@ -1,1 +1,16 @@
-
+/*==================Check Positive or Negative Number Program, ( if,else ) Statement==================*/
+import java.util.Scanner;
+public class Main{
+public static void main (String[] args) {
+    int $num;
+    Scanner input = new Scanner(System.in);
+    System.out.print("Enter a integer number: ");
+    $num = input.nextInt();
+    if($num > 0){
+        System.out.println("This number is Positive");
+    }
+    else{
+        System.out.println("This number is Negative");
+    }
+}
+}
