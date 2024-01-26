0833dd64ddc89d38a1a1afafb4ccae1759901e25
hasancoders09 <145287137+hasancoders09@users.noreply.github.com>
2024-01-27 01:14:04 +0600

    Update Main.java

--- 10 Control Statement/Main.java
+++ 10 Control Statement/Main.java
@@ -1,1 +1,16 @@
+/*==================Control Statement=================
+Selection Statement
+if, if-else, switch
 
+Looping Statement
+for, while, do-while
+
+Jump Statement
+Break, continue, return
+
+========================================================*/
+public class Main{
+public static void main (String[] args) {
+    /* code */
+}
+}
