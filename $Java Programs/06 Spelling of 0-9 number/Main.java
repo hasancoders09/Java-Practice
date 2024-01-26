50e6d5a20f6204eb8a1d469e89838af60862780e
hasancoders09 <145287137+hasancoders09@users.noreply.github.com>
2024-01-27 03:47:06 +0600

    Update Main.java

--- $Java Programs/06 Spelling of 0-9 number/Main.java
+++ $Java Programs/06 Spelling of 0-9 number/Main.java
@@ -1,1 +1,46 @@
+/*=====Spelling of 0-9 number Program, ( if,else ) Statement=====*/
+import java.util.Scanner;
 
+public class Main{
+    public static void main(String[] args){
+        Scanner $input = new Scanner(System.in);
+        int $number;
+        
+        System.out.print("Enter number within 0-9: ");
+        $number = $input.nextInt();
+        
+        if($number == 0){
+            System.out.println("Zero");
+        }
+        else if($number == 1){
+            System.out.println("One");
+        }
+        else if($number == 2){
+            System.out.println("Two");
+        }
+        else if($number == 3){
+            System.out.println("Three");
+        }
+        else if($number == 4){
+            System.out.println("Four");
+        }
+        else if($number == 5){
+            System.out.println("Five");
+        }
+        else if($number == 6){
+            System.out.println("Six");
+        }
+        else if($number == 7){
+            System.out.println("Seven");
+        }
+        else if($number == 8){
+            System.out.println("Eight");
+        }
+        else if($number == 9){
+            System.out.println("Nine");
+        }
+        else{
+            System.out.println("Sorry...! Invalid Number");
+        }
+    }
+}
