0499ce057cca447df8e5635cac87b30e9f656fdb
hasancoders09 <145287137+hasancoders09@users.noreply.github.com>
2024-01-27 00:27:03 +0600

    Update Main.java

--- 09 Relational Operator/Main.java
+++ 09 Relational Operator/Main.java
@@ -1,1 +1,23 @@
+/*==================Relational Operator=================
+Operator	Description	                Example
+==	        Is Equal To	                3 == 5 returns false
+!=	        Not Equal To	            3 != 5 returns true
+>	        Greater Than	            3 > 5 returns false
+<	        Less Than	                3 < 5 returns true
+>=	        Greater Than or Equal To	3 >= 5 returns false
+<=	        Less Than or Equal To	    3 <= 5 returns true
+
+========================================================*/
+public class Main
+{
+	public static void main(String[] args) {
+		int $num_1 = 5, $num_2 = 9;
+		System.out.println($num_1 == $num_2);//false
+		System.out.println($num_1 != $num_2);//true
+		System.out.println($num_1 > $num_2);//false
+		System.out.println($num_1 < $num_2);//true
+		System.out.println($num_1 >= $num_2);//false
+		System.out.println($num_1 <= $num_2);//true
+	}
+}
 
