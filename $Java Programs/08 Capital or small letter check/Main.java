e602d559375ad4372fecaee49b33b549a1688022
hasancoders09 <145287137+hasancoders09@users.noreply.github.com>
2024-01-27 23:17:22 +0600

    Update Main.java

--- $Java Programs/08 Capital or small letter check/Main.java
+++ $Java Programs/08 Capital or small letter check/Main.java
@@ -1,1 +1,20 @@
+/* ===================== Capital or small letter check program ================== */
 
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args){
+        Scanner $inputChar = new Scanner(System.in);
+        char $letter;
+        System.out.print("Enter a letter: ");
+        $letter = $inputChar.next().charAt(0);
+        if($letter >= 'A' && $letter <= 'Z'){
+            System.out.println("Capital letter");
+        }
+        else if($letter >= 'a' && $letter <= 'z'){
+            System.out.println("Small letter");
+        }
+        else{
+            System.out.println("Not a letter");
+        }
+    }
+}
