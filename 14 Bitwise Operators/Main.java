285b5e8ccf3e0dde6559c10c9c9ba0c862d25a1b
hasancoders09 <145287137+hasancoders09@users.noreply.github.com>
2024-01-29 03:29:28 +0600

    Update Main.java

--- 14 Bitwise Operators/Main.java
+++ 14 Bitwise Operators/Main.java
@@ -3,8 +3,8 @@
 
 Operators	                    Symbol	    Uses
 Bitwise AND	                    &	        op1 & op2
-Bitwise exclusive OR	        ^	        op1 ^ op2
 Bitwise inclusive OR	        |	        op1 | op2
+Bitwise exclusive OR	        ^	        op1 ^ op2
 Bitwise Compliment	            ~	        ~ op
 Bitwise left shift	            <<	        op1 << op2
 Bitwise right shift	            >>	        op1 >> op2
@@ -14,6 +14,33 @@ Unsigned Right Shift Operator	>>>         op >>>	number of places to shift
 public class Main
 {
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		int $num_1 = 17, $num_2 = 20, $result;
+		
+		//Bitwise And (&)
+		$result = $num_1 & $num_2;
+		/*=====================================
+		$num_1: 17 = 10001
+		$num_2: 20 = 10100
+		$result      10000 = 16
+		=======================================*/
+		System.out.println("Result of 'Bitwise And': " + $result);
+		
+		//Bitwise Or (|)
+		$result = $num_1 | $num_2;
+		/*=====================================
+		$num_1: 17 = 10001
+		$num_2: 20 = 10100
+		$result      10101 = 21
+		=======================================*/
+		System.out.println("Result of 'Bitwise Or': " + $result);
+		
+		//Bitwise ExOR (^)
+		$result = $num_1 ^ $num_2;
+		/*=====================================
+		$num_1: 17 = 10001
+		$num_2: 20 = 10100
+		$result      00101 = 5
+		=======================================*/
+		
 	}
 }
