ad1b6e11ff29eaf0e2aefa665a3a4bc378043c00
hasancoders09 <145287137+hasancoders09@users.noreply.github.com>
2024-01-28 23:26:24 +0600

    Update Main.java

--- 14 Bitwise Operators/Main.java
+++ 14 Bitwise Operators/Main.java
@@ -1,1 +1,19 @@
+/******************************************************************************
+===================== Bitwise Operators ======================
 
+Operators	                    Symbol	    Uses
+Bitwise AND	                    &	        op1 & op2
+Bitwise exclusive OR	        ^	        op1 ^ op2
+Bitwise inclusive OR	        |	        op1 | op2
+Bitwise Compliment	            ~	        ~ op
+Bitwise left shift	            <<	        op1 << op2
+Bitwise right shift	            >>	        op1 >> op2
+Unsigned Right Shift Operator	>>>         op >>>	number of places to shift
+
+*******************************************************************************/
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+}
