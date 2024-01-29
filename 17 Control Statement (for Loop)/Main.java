e21068d8799d081dd37a8ccd92e89020f9123a74
hasancoders09 <145287137+hasancoders09@users.noreply.github.com>
2024-01-30 05:17:28 +0600

    Update Main.java

--- 17 Control Statement (for Loop)/Main.java
+++ 17 Control Statement (for Loop)/Main.java
@@ -1,1 +1,20 @@
+/*====================For loop======================*/
+/*=========================================
+3 parts of (for loop)
+1/ initialization
+2/ Condition
+3/ Increment/ decrement part
+===========================================*/
+public class Main
+{
+	public static void main(String[] args) {
+		for(int i = 1; i <= 10; i++){
+		    System.out.println(i);
+		}
+		
+		for(int j = 5; j<= 20; j=j+2){
+		    System.out.println(j);
+		}
+	}
+}
 
