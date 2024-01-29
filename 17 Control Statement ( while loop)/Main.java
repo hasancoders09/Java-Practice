d0f4d5a80fa9fa7e34c549753e11d78bc33f2b44
hasancoders09 <145287137+hasancoders09@users.noreply.github.com>
2024-01-30 05:35:57 +0600

    Update Main.java

--- 17 Control Statement ( while loop)/Main.java
+++ 17 Control Statement ( while loop)/Main.java
@@ -1,1 +1,16 @@
-
+/*====================While loop======================*/
+/*=========================================
+3 parts of (for loop)
+1/ initialization
+2/ Condition
+3/ Increment/ decrement part
+===========================================*/
+public class Main{
+    public static void main(String[] args){
+        int i = 1;
+        while(i<=100){
+            System.out.println(i);
+            i = i+2;
+        }
+    }
+}
