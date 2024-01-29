b0e238c98a4f441f3a81d141c6f00ef56e77285b
hasancoders09 <145287137+hasancoders09@users.noreply.github.com>
2024-01-30 05:59:11 +0600

    Update Main.java

--- 19 Control Statement ( do while loop)/Main.java
+++ 19 Control Statement ( do while loop)/Main.java
@@ -1,1 +1,16 @@
-
+/*====================do While loop======================*/
+/*=========================================
+3 parts of (for loop)
+1/ initialization
+2/ Condition
+3/ Increment/ decrement part
+===========================================*/
+public class Main{
+    public static void main(String[] args){
+        int i = 1;
+        do{
+            System.out.println(i+" Bangladesh");
+            i++;
+        }while(i<=10);
+    }
+} 
