5f9b305200db19489a7699b7d9f5ed0dbf2dd69e
hasancoders09 <145287137+hasancoders09@users.noreply.github.com>
2024-01-30 06:04:40 +0600

    Update Main.java

--- 20 while vs do-while loop/Main.java
+++ 20 while vs do-while loop/Main.java
@@ -1,1 +1,20 @@
-
+/*====================while vs do-while loop======================*/
+/*=========================================
+3 parts of (for loop)
+1/ initialization
+2/ Condition
+3/ Increment/ decrement part
+===========================================*/
+public class Main{
+    public static void main(String[] args){
+        int x = 6;
+        while(x<=5){
+            System.out.println(x+" while loop");//it not run the first loop
+        }
+        int i = 6;
+        do{
+            System.out.println(i+" do while loop"); //it minimum run at first loop
+            i++;
+        }while(i<=5);
+    }
+} 
