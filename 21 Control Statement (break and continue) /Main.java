720ca945c777ff4631010a63d259729ad17a6392
hasancoders09 <145287137+hasancoders09@users.noreply.github.com>
2024-01-31 11:02:13 +0600

    Update Main.java

--- 21 Control Statement (break and continue) /Main.java
+++ 21 Control Statement (break and continue) /Main.java
@@ -1,1 +1,16 @@
+/*================================break and continue statement===============================*/
+public class Main
+{
+	public static void main(String[] args) {
+		for(int $num = 1; $num <= 100; $num++){
+		    if($num == 10)break;//It's break the loop in 10
+		    System.out.println("break example: "+$num);
+		}
+		for(int $num = 1; $num <= 12; $num++){
+		    if($num == 10)continue;// It's continue the loop 10
+		    System.out.println("continue example"+$num);
+		}
+		
+	}
+}
 
