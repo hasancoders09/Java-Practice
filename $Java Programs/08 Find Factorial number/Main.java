b5637b6dbae50a3e3a693ffc38508222ce6f9c35
hasancoders09 <145287137+hasancoders09@users.noreply.github.com>
2024-02-01 06:49:46 +0600

    Update Main.java

--- $Java Programs/08 Find Factorial number/Main.java
+++ $Java Programs/08 Find Factorial number/Main.java
@@ -1,1 +1,16 @@
+/*========================Find Factorial number =======================*/
 
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] arge){
+        Scanner $input= new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n, $fact = 1;
+        n = $input.nextInt();
+        for(int i = 1; i <= n; i++){
+            $fact = $fact*i; 
+        }
+        System.out.printf("Factorial of %d is: ", n);
+        System.out.println($fact);
+    }
+}
