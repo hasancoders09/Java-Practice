81fdb2ee8f147a177480df69c6b6c928d916452c
hasancoders09 <145287137+hasancoders09@users.noreply.github.com>
2024-02-01 07:33:32 +0600

    Update Main.java

--- $Java Programs/10 Sum of odd number n to m/Main.java
+++ $Java Programs/10 Sum of odd number n to m/Main.java
@@ -1,1 +1,18 @@
-
+/*========================Sum of odd number n to m =======================*/
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args){
+        Scanner $input = new Scanner(System.in);
+        int n,m,$sum=0;
+        System.out.print("Enter Starting number: ");
+        n = $input.nextInt();
+        System.out.print("Enter End number: ");
+        m = $input.nextInt();
+        for(int i = n; i<=m; i++){
+            if(i%2 != 0){
+                $sum = $sum + i;
+            }
+        }
+        System.out.println("Result: " + $sum);
+    }
+}
